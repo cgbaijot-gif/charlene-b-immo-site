6752e9241dacd897c031377eb118b59e345282af
cgbaijot-gif <cgbaijot@gmail.com>
2026-05-10 18:34:20 +0200

    Create Header.tsx

---
+++ app/app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="w-full py-6 bg-white shadow-sm">
+      <div className="max-w-5xl mx-auto px-4">
+        <h2 className="text-xl font-bold text-or">Charlene B Immobilier</h2>
+      </div>
+    </header>
+  );
+}
